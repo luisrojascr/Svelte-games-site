e61f53173b0e83a453b3d26f749659043b67e32e
dev1 <dev1@infinitro.dev>
2023-11-16 19:13:19 -0800

    modal dialog template

---
+++ libs/_modal/src/lib/locale/translations.ts
@@ -0,0 +1,27 @@
+import cs from './edited/cs.json';
+import de from './edited/de.json';
+import en from './edited/en.json';
+import es from './edited/es.json';
+import fr from './edited/fr.json';
+import it from './edited/it.json';
+import ja from './edited/ja.json';
+import pt from './edited/pt.json';
+import ru from './edited/ru.json';
+import zh from './edited/zh.json';
+import type { KeyValues, Translation } from './types';
+export type { KeyValues, Translation };
+
+const translations: Translation = {
+	cs,
+	de,
+	en,
+	es,
+	fr,
+	it,
+	ja,
+	pt,
+	ru,
+	zh
+};
+
+export default translations;
