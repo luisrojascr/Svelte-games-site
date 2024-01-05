50474d823a3bd94a0403216265c51cbc1e05a38d
Luis Rojas <luis@coinz.studio>
2024-01-05 00:47:15 -0600

    ApeCrash game fixes

--- libs/ape-game/src/lib/parts/game.svelte
+++ libs/ape-game/src/lib/parts/game.svelte
@@ -7,8 +7,7 @@
 	import Layer2 from '$lib/ape/zone1-layer2.svg';
 	import Layer3 from '$lib/ape/zone1-layer3.svg';
 	import Layer4 from '$lib/ape/zone1-layer4.svg';
-	import { onDestroy } from 'svelte';
-	import { cubicOut } from 'svelte/easing';
+	import { onDestroy, onMount } from 'svelte';
 	import { tweened } from 'svelte/motion';
 
 	let playing = false;
@@ -17,28 +16,34 @@
 	let showFirstApeCrash = true;
 	let crashInterval: NodeJS.Timeout | undefined;
 	let crashTimeout: NodeJS.Timeout | undefined;
+	let gameContainer: HTMLElement | undefined;
+	let gameContainerWidth: number;
 	const pauseDuration = 2000;
-	const durationTime = 120000;
+	const durationTime = 80000;
 
 	let backgroundLastVal = 0;
 	let midgroundLastVal = 0;
 	let midfrontgroundLastVal = 0;
 	let foregroundLastVal = 0;
 
-	let background = tweened(backgroundLastVal, { duration: durationTime, easing: cubicOut });
-	let midground = tweened(midgroundLastVal, { duration: durationTime, easing: cubicOut });
-	let midfrontground = tweened(midfrontgroundLastVal, { duration: durationTime, easing: cubicOut });
-	let foreground = tweened(foregroundLastVal, { duration: durationTime, easing: cubicOut });
+	let background = tweened(backgroundLastVal, { duration: durationTime });
+	let midground = tweened(midgroundLastVal, { duration: durationTime });
+	let midfrontground = tweened(midfrontgroundLastVal, {
+		duration: durationTime
+	});
+	let foreground = tweened(foregroundLastVal, { duration: durationTime });
 
 	const setAnimation = (value: number) => {
 		background.set(value);
-		midground.set(value * 1.6283);
-		midfrontground.set(value * 3.5614); // 14370
-		foreground.set(value * 5.0211); // 20260
+		midground.set(value * 1.6283); // 7690 - 1504
+		midfrontground.set(value * 3.5614); // 14370 --- 15490 - 1504
+		foreground.set(value * 5.5348); // 20260
 	};
 
 	const startAnimation = () => {
-		setAnimation(paused ? backgroundLastVal : -4035);
+		console.log('gameContainerWidth: ', gameContainerWidth);
+		const backgroundWidthValue = 5155 - gameContainerWidth; // Layer1 SVG width - gameContainerWidth
+		setAnimation(paused ? backgroundLastVal : -backgroundWidthValue);
 		playing = true;
 		paused = false;
 	};
@@ -68,7 +73,7 @@
 		startAnimation();
 	};
 
-	const handleRain = () => {
+	const handleCrash = () => {
 		if (playing) {
 			pause();
 			crashing = true;
@@ -84,13 +89,29 @@
 		crashInterval = setInterval(() => (showFirstApeCrash = !showFirstApeCrash), 3000);
 	};
 
+	const calculateWidth = (): number => {
+		if (!gameContainer) {
+			console.error('The game-container element has not been rendered yet.');
+			return 0;
+		}
+
+		const style = getComputedStyle(gameContainer);
+		return parseFloat(style.width);
+	};
+
+	onMount(() => {
+		// Call the calculateWidth function after the component mounts
+		gameContainerWidth = calculateWidth();
+		// Optional: do something with the width
+	});
+
 	onDestroy(() => {
 		clearInterval(crashInterval);
 		clearTimeout(crashTimeout);
 	});
 </script>
 
-<div class="game-container">
+<div bind:this={gameContainer} class="game-container">
 	<div class="ape-container">
 		<span class="rope"></span>
 		{#if crashing}
@@ -126,7 +147,7 @@
 </div>
 <div class="buttons">
 	<button class="play-button" on:click={startAnimation}> Start Animation </button>
-	<button class="rain-button" on:click={handleRain}>Make it rain</button>
+	<button class="rain-button" on:click={handleCrash}>Make it crash</button>
 	<button class="reset-button" on:click={resetAnimation}>Reset</button>
 </div>
 
@@ -134,20 +155,16 @@
 	.game-container {
 		@apply relative overflow-hidden w-full h-[420px];
 	}
-
 	.game-container img {
 		@apply max-w-fit absolute bottom-0;
 	}
-
 	.ape-container {
 		@apply relative top-[32%] z-10;
 	}
-
 	.ape,
 	.ape-crashing {
 		@apply z-10 w-[60px] top-1/2 left-1/2;
 	}
-
 	.ape-crashing {
 		transition: opacity 0.5s ease;
 		opacity: 1;
@@ -155,34 +172,30 @@
 		top: -19px;
 		left: 47%;
 	}
-
 	.rope {
 		@apply bg-[#a9a9a9] h-[3px] w-full block top-0;
 	}
-
 	.crash {
 		@apply relative top-[37%] z-10 left-[53%];
 	}
-
 	.crash img {
 		@apply w-[90px];
 	}
-
 	.buttons {
 		@apply flex flex-row;
 	}
-
 	.play-button,
 	.rain-button,
 	.reset-button {
 		@apply mt-4 bg-green-600 text-white px-4 py-2 rounded-md transition-colors mt-3 ml-0 text-sm px-6;
 	}
-
 	.rain-button {
 		@apply ml-3 mr-3;
 	}
-
 	.layer {
 		@apply absolute w-full h-full bottom-0 left-0 will-change-transform;
+	}
+	.foreground-layer {
+		@apply z-20;
 	}
 </style>
