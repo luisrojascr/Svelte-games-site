48e378c7ff4aaba33f6020929aa42c838e2a11ae
Luis Rojas <luis@coinz.studio>
2024-01-17 23:51:52 -0600

    Update Ape Crash game

--- libs/ape-game/src/lib/parts/game.svelte
+++ libs/ape-game/src/lib/parts/game.svelte
@@ -37,105 +37,84 @@
 	let betMultiplier = 1.1;
 	let animationFrameId: number | undefined;
 
-	console.log(window.innerWidth);
+	$: probabilityPercentage = calculateProbability(betMultiplier).toFixed(2) + '%';
+
+	const calculateForegroundMultiplier = (): number => {
+		const screenWidth = window.innerWidth;
+
+		if (screenWidth <= 1535) return 5.204;
+		if (screenWidth <= 1279) return 4.915;
+		if (screenWidth <= 1023) return 4.659;
+		if (screenWidth <= 767) return 4.542;
+		if (screenWidth <= 640) return 4.516;
+		if (screenWidth <= 567) {
+			const baseSize = 567;
+			const baseMultiplier = 4.478;
+			const reductionPer10px = 0.00835;
+			const sizeDifference = baseSize - screenWidth;
+			const multiplierAdjustment = Math.floor(sizeDifference / 10) * reductionPer10px;
+			return baseMultiplier - multiplierAdjustment;
+		}
+		return 5.5348; // Default
+	};
+
+	const calculateProbability = (multiplier: number): number => {
+		// Define a base multiplier and the corresponding probability of not crashing
+		const baseMultiplier = 1.0;
+		const baseProbability = 1.0; // 100% chance of not crashing at 1.0x
+		const decreasePerStep = 0.0001;
+		const stepsAboveBase = (multiplier - baseMultiplier) / 0.01;
+		let currentProbability = baseProbability - stepsAboveBase * decreasePerStep;
+		currentProbability = Math.max(currentProbability, 0);
+		return currentProbability;
+	};
+
+	const shouldCrash = (): boolean => {
+		const notCrashingProbability = calculateProbability(multiplier);
+		const randomChance = Math.random();
+		return randomChance > notCrashingProbability; // Crash if the random chance exceeds the probability of not crashing
+	};
 
 	const setAnimation = (value: number) => {
 		background.set(value);
 		midground.set(value * 1.6283); // 7690 - 1504
-		midfrontground.set(value * 3.5614); // 14370 --- 15490 - 1504
+		midfrontground.set(value * 3.5614); // 15490 - 1504
 		// foreground.set(value * 5.5348); // 20260
 
-		let foregroundMultiplier;
-		if (window.innerWidth <= 1535 && window.innerWidth >= 1280) {
-			foregroundMultiplier = 5.204; // Value for screens between 1280px and 1535px
-		} else if (window.innerWidth <= 1279 && window.innerWidth >= 1024) {
-			foregroundMultiplier = 4.915; // Value for screens between 1024px and 1279px
-		} else if (window.innerWidth <= 1023 && window.innerWidth >= 768) {
-			foregroundMultiplier = 4.659; // Value for screens 1023px or smaller
-		} else if (window.innerWidth <= 767 && window.innerWidth >= 640) {
-			foregroundMultiplier = 4.542; // Value for screens 767px or smaller
-		} else if (window.innerWidth <= 640 && window.innerWidth >= 600) {
-			foregroundMultiplier = 4.516; // Value for screens 640px or smaller
-		} else if (window.innerWidth <= 567) {
-			// Dynamically calculate foregroundMultiplier for screens smaller than 567px
-			const baseSize = 567;
-			const baseMultiplier = 4.478;
-			const reductionPer10px = 0.00835;
-			const sizeDifference = baseSize - window.innerWidth;
-			const multiplierAdjustment = Math.floor(sizeDifference / 10) * reductionPer10px;
-			foregroundMultiplier = baseMultiplier - multiplierAdjustment;
-		} else {
-			foregroundMultiplier = 5.5348; // Default value for screens larger than 1535px
-		}
-		foreground.set(value * foregroundMultiplier); // Apply the calculated multiplier
+		const foregroundMultiplier = calculateForegroundMultiplier();
+		foreground.set(value * foregroundMultiplier);
 	};
 
-	// Function to calculate the crash probability based on the multiplier
-	function calculateProbability(multiplier: number): number {
-		// Define a base multiplier and the corresponding probability of not crashing
-		const baseMultiplier = 1.0;
-		const baseProbability = 1.0; // 100% chance of not crashing at 1.0x
+	const animate = () => {
+		if (playing && !crashing) {
+			// Increase the multiplier
+			multiplier += 0.01;
 
-		// Calculate the decrease in the probability of not crashing per step
-		const decreasePerStep = 0.0001;
+			// Check if the game should crash
+			if (shouldCrash()) {
+				crashing = true;
+				handleCrash();
+			} else {
+				const backgroundWidthValue = 5155 - gameContainerWidth;
+				setAnimation(paused ? backgroundLastVal : -backgroundWidthValue);
+				animationFrameId = requestAnimationFrame(animate);
+			}
+		}
+	};
 
-		// Calculate the number of steps above the base multiplier
-		const stepsAboveBase = (multiplier - baseMultiplier) / 0.01;
+	const resetGameState = () => {
+		// Stop any existing animation frame
+		if (animationFrameId) {
+			cancelAnimationFrame(animationFrameId);
+		}
 
-		// Calculate the current probability of not crashing
-		let currentProbability = baseProbability - stepsAboveBase * decreasePerStep;
-
-		// Ensure the probability doesn't fall below zero
-		currentProbability = Math.max(currentProbability, 0);
-		console.log(currentProbability);
-
-		return currentProbability;
-	}
-
-	// Function to calculate the probability percentage
-	// function calculateProbability(multiplier: number): number {
-	// 	// Base values at 1.01x
-	// 	const baseMultiplier = 1.01;
-	// 	const baseProbability = 98.02; // 98.02% at 1.01x
-
-	// 	// Calculate the decrease rate per 0.01 increase in multiplier
-	// 	const decreasePerStep = 0.96; // Adjust this rate to match your actual data
-
-	// 	// Calculate how many steps above the base multiplier
-	// 	const steps = (multiplier - baseMultiplier) / 0.01;
-
-	// 	// Calculate and return the current probability
-	// 	return baseProbability - steps * decreasePerStep;
-	// }
-
-	$: probabilityPercentage = calculateProbability(betMultiplier).toFixed(2) + '%';
-
-	// Function to decide if the game should crash at the current multiplier
-	function shouldCrash(): boolean {
-		const notCrashingProbability = calculateProbability(multiplier);
-		const randomChance = Math.random();
-		return randomChance > notCrashingProbability; // Crash if the random chance exceeds the probability of not crashing
-	}
+		multiplier = 1.0;
+		crashing = false;
+		paused = false;
+	};
 
 	const startAnimation = () => {
 		resetGameState(); // Reset the game state before starting
-
-		function animate() {
-			if (playing && !crashing) {
-				// Increase the multiplier
-				multiplier += 0.01;
-
-				// Check if the game should crash
-				if (shouldCrash()) {
-					crashing = true;
-					handleCrash();
-				} else {
-					const backgroundWidthValue = 5155 - gameContainerWidth;
-					setAnimation(paused ? backgroundLastVal : -backgroundWidthValue);
-					animationFrameId = requestAnimationFrame(animate);
-				}
-			}
-		}
 
 		// Start the animation loop
 		animationFrameId = requestAnimationFrame(animate);
@@ -143,11 +122,29 @@
 		paused = false;
 	};
 
-	const resume = () => {
-		startAnimation();
+	const resetAnimation = () => {
+		playing = false;
+		paused = false;
+		crashing = false;
+		multiplier = 1.0;
+		clearInterval(crashInterval);
+		clearTimeout(crashTimeout);
+
+		background.set(0, { duration: 100 });
+		midground.set(0, { duration: 100 });
+		midfrontground.set(0, { duration: 100 });
+		foreground.set(0, { duration: 100 });
 	};
 
-	// Handle the game crash
+	const increaseBet = () => {
+		betMultiplier += 0.1;
+	};
+	const decreaseBet = () => {
+		if (betMultiplier > 1.1) {
+			betMultiplier -= 0.1;
+		}
+	};
+
 	const handleCrash = () => {
 		if (playing) {
 			crashing = true;
@@ -166,31 +163,6 @@
 		}
 	};
 
-	function resetGameState() {
-		// Stop any existing animation frame
-		if (animationFrameId) {
-			cancelAnimationFrame(animationFrameId);
-		}
-
-		multiplier = 1.0;
-		crashing = false;
-		paused = false;
-	}
-
-	const resetAnimation = () => {
-		playing = false;
-		paused = false;
-		crashing = false;
-		multiplier = 1.0;
-		clearInterval(crashInterval);
-		clearTimeout(crashTimeout);
-
-		background.set(0, { duration: 100 });
-		midground.set(0, { duration: 100 });
-		midfrontground.set(0, { duration: 100 });
-		foreground.set(0, { duration: 100 });
-	};
-
 	const toggleApeCrashImage = () => {
 		crashInterval = setInterval(() => (showFirstApeCrash = !showFirstApeCrash), 3000);
 	};
@@ -204,16 +176,6 @@
 		const style = getComputedStyle(gameContainer);
 		return parseFloat(style.width);
 	};
-
-	function increaseBet() {
-		betMultiplier += 0.1;
-	}
-
-	function decreaseBet() {
-		if (betMultiplier > 1.1) {
-			betMultiplier -= 0.1;
-		}
-	}
 
 	onMount(() => {
 		// Call the calculateWidth function after the component mounts
@@ -275,7 +237,7 @@
 
 <div class="buttons">
 	<button class="play-button" on:click={startAnimation}> Start Animation </button>
-	<button class="rain-button" on:click={handleCrash}>Make it crash</button>
+	<!-- <button class="rain-button" on:click={handleCrash}>Make it crash</button> -->
 	<button class="reset-button" on:click={resetAnimation}>Reset</button>
 </div>
 
@@ -320,30 +282,24 @@
 	.rain-button {
 		@apply ml-3 mr-3;
 	}
-
 	.layer {
 		@apply absolute w-full h-full bottom-0 left-0 will-change-transform;
 	}
 	.foreground-layer {
 		@apply z-20;
 	}
-
 	.multiplier-display {
 		@apply text-white text-4xl;
 	}
-
 	.multiplier-container {
 		@apply flex items-center gap-6;
 	}
-
 	.multiplier-button {
 		@apply my-4 bg-gray-700 text-white py-2 rounded-md transition-colors ml-0 text-sm px-6;
 	}
-
 	.multiplier-count {
 		@apply text-xl text-white;
 	}
-
 	.probability-count {
 		@apply text-base text-white;
 	}
